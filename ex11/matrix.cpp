cde474747909fb5c5723ac58ed31c37fb1773e44
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2018-12-18 18:50:54 +0200

    Ex11: Passing matrix in a function

--- ex11/matrix.cpp
+++ ex11/matrix.cpp
@@ -11,25 +11,8 @@ void solution() {
 
 }
 
-bool isWordMatch(char text[], int start, char word[]) {
-  int wordLen = strlen(word);
-  for (int i = 0; i < wordLen; i++) {
-    if (text[start + i] != word[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+void solution(int a[100][100], int n, int m) {
 
-int numberOfOccurrences(char text[], char word[]) {
-  int textLen = strlen(text);
-  int occurrences = 0;
-  for (int i = 0; i <= textLen; i++) {
-    if (isWordMatch(text, i, word)) {
-      occurrences++;
-    }
-  }
-  return occurrences;
 }
 
 int main() {
